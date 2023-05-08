909bfdf08e038fc89e88ec181f80b368925271f6
colleenInKorea <92035417+colleenInKorea@users.noreply.github.com>
2023-05-08 20:50:22 +0900

    프로그래머스 문자열 분리하기 Test04

---
+++ src/Day03/Test04/Solution.java
@@ -0,0 +1,12 @@
+package Day03.Test04;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        for(int i = 0; i < a.length(); i++){
+            System.out.println(a.charAt(i));
+        }
+    }
+}
