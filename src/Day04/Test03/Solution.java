fbb6902ecf9021f41553517e5c6e47bdfdf6dcab
colleenInKorea <92035417+colleenInKorea@users.noreply.github.com>
2023-05-09 20:53:19 +0900

    프로그래머스 문자 리스트를 문자열로 변환하기 Test03

---
+++ src/Day04/Test03/Solution.java
@@ -0,0 +1,11 @@
+package Day04.Test03;
+
+class Solution {
+    public String solution(String[] arr) {
+        String answer = "";
+        for (int i = 0; i < arr.length; i++){
+            answer += arr[i].toString();
+        }
+        return answer;
+    }
+}
