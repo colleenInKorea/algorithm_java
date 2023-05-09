b2205356e757ee41433ecf913a045df844a39ff4
colleenInKorea <92035417+colleenInKorea@users.noreply.github.com>
2023-05-09 20:55:15 +0900

    프로그래머스 문자열 곱하기 Test04

---
+++ src/Day04/Test04/Solution.java
@@ -0,0 +1,11 @@
+package Day04.Test04;
+
+class Solution {
+    public String solution(String my_string, int k) {
+        String answer = "";
+        for (int i = 0; i < k; i++){
+            answer += my_string;
+        }
+        return answer;
+    }
+}
