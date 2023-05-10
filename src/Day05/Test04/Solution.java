1c9607d7be4c9af2059f6f64b038d2eb8a0b0e46
colleenInKorea <92035417+colleenInKorea@users.noreply.github.com>
2023-05-10 22:16:53 +0900

    프로그래머스 공배수 Test04

---
+++ src/Day05/Test04/Solution.java
@@ -0,0 +1,13 @@
+package Day05.Test04;
+
+class Solution {
+    public int solution(int number, int n, int m) {
+        int answer = 0;
+        if(number % n == 0 && number % m == 0){
+            answer = 1;
+        }else {
+            answer = 0;
+        }
+        return answer;
+    }
+}
