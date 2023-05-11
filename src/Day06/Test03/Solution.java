c93ec34394d883f0e0f5115accd3aab4bfab8cb9
colleenInKorea <92035417+colleenInKorea@users.noreply.github.com>
2023-05-11 23:46:45 +0900

    프로그래머스 등차수열의 특정한 항만 더하기 Test03 - 불필요한 코드 제거

--- src/Day06/Test03/Solution.java
+++ src/Day06/Test03/Solution.java
@@ -3,11 +3,9 @@ package Day06.Test03;
 class Solution {
     public int solution(int a, int d, boolean[] included) {
         int answer = 0;
-        int result = 0;
         for (int i = 0; i < included.length; i++){
-            result = a + (i * d);
             if (included[i] == true) {
-                answer += result;
+                answer += a + (i * d);
             }
         }
         return answer;
