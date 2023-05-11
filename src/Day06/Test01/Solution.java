5d1ee65359c1d4324a694adead716426dd1ce0dc
colleenInKorea <92035417+colleenInKorea@users.noreply.github.com>
2023-05-11 23:36:07 +0900

    프로그래머스 flag에 따라 다른 값 반환하기 Test01

---
+++ src/Day06/Test01/Solution.java
@@ -0,0 +1,13 @@
+package Day06.Test01;
+
+class Solution {
+    public int solution(int a, int b, boolean flag) {
+        int answer = 0;
+        if(flag == true){
+            answer = a + b;
+        }else {
+            answer = a - b;
+        }
+        return answer;
+    }
+}
