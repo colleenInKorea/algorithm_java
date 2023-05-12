a8da02a74698435ab4f511a8da34513aaf92d083
colleenInKorea <92035417+colleenInKorea@users.noreply.github.com>
2023-05-13 00:06:59 +0900

    프로그래머스 이어 붙인 수 Test02

---
+++ src/Day07/Test02/Solution.java
@@ -0,0 +1,18 @@
+package Day07.Test02;
+
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        String num1 = "";
+        String num2 = "";
+        for (int i = 0; i < num_list.length; i++){
+            if(num_list[i] % 2 == 0){
+                num1 += Integer.toString(num_list[i]);
+            }else {
+                num2 += Integer.toString(num_list[i]);
+            }
+        }
+        answer = Integer.parseInt(num1) + Integer.parseInt(num2);
+        return answer;
+    }
+}
