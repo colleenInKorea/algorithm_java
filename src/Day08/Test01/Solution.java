1a7a4f0fd0e64d49c46808619c1c7f1c3c850c75
colleenInKorea <92035417+colleenInKorea@users.noreply.github.com>
2023-05-14 23:33:59 +0900

    프로그래머스 마지막 두 원소 Test01

---
+++ src/Day08/Test01/Solution.java
@@ -0,0 +1,17 @@
+package Day08.Test01;
+
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        int idxLast = num_list.length;
+        int[] answer = new int[idxLast + 1];
+        System.arraycopy(num_list, 0, answer, 0, idxLast);
+
+        answer[idxLast] = answer[idxLast - 1] > answer[idxLast - 2]
+                ? answer[idxLast - 1] - answer[idxLast - 2]
+                : answer[idxLast - 1] * 2;
+        return answer;
+    }
+}
