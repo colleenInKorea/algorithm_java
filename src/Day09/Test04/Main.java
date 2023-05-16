5e68b0c4e4ef6900348618cc5f8c772e101547d0
colleenInKorea <92035417+colleenInKorea@users.noreply.github.com>
2023-05-16 21:07:39 +0900

    백준 Test04

---
+++ src/Day09/Test04/Main.java
@@ -0,0 +1,16 @@
+package Day09.Test04;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        BigInteger a = in.nextBigInteger();
+        BigInteger b = in.nextBigInteger();
+
+        System.out.println(a.add(b));
+        System.out.println(a.subtract(b));
+        System.out.println(a.multiply(b));
+    }
+}
