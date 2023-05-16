5b4d6bc355855b676a14264517e336d30e018f12
colleenInKorea <92035417+colleenInKorea@users.noreply.github.com>
2023-05-16 21:07:30 +0900

    백준 Test03

---
+++ src/Day09/Test03/Main.java
@@ -0,0 +1,12 @@
+package Day09.Test03;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("(___)\n" +
+                "(o o)____/\n" +
+                " @@      \\\n" +
+                "  \\ ____,/\n" +
+                "  //   //\n" +
+                " ^^   ^^");
+    }
+}
